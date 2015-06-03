e4f7d8ca71c6714e8e2ce99345a88ec72fb2e767
Ethan Ensler <ejensler@gmail.com>
2015-06-03 00:04:57 -0700

    Basic search functionality, with results limit and debounce.

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 // Bootstrap app with top-level controller
-angular.module('places', ['ejAngularSearchPlaces'])
+angular.module('places', ['angularSearchPlaces'])
   .controller('PlacesSearchController', PlacesSearchController);
 
-function PlacesSearchController() {
+function PlacesSearchController($window) {
   this.city = "San Francisco";
+  this.searchTerms = "";
 }
 
-angular.module('ejAngularSearchPlaces', [])
+PlacesSearchController.prototype.go = function() {
+  if (this.searchTerms) {
+    $window.alert("Do something with the search result!");
+  }
+};
 
-.directive('searchPlacesInput', function() {
+angular.module('angularSearchPlaces', [])
+
+.directive('ejSearchPlacesInput', ['$compile', '$timeout',
+            function($compile, $timeout) {
 /**
  *  In order to generate Nearby Search results, a location must be specified.
  *  (https://developers.google.com/maps/documentation/javascript/places#place_search_requests)
@@ -23,65 +31,97 @@ angular.module('ejAngularSearchPlaces', [])
   // Create the LatLngBounds object
   var SAN_FRANCISCO = new google.maps.LatLngBounds(SF_SW, SF_NE);
 
+  function link($scope, $el, $attrs, ngModelCtrl) {
+
+    // Intialize options
+
+    // Set the limit on search results to a reasonable number by default
+    var resultsLimit = $scope.$eval($attrs.searchResultsLimit) || 8;
+
+    // Set a debounce on updating the model so we're not sending tons of requests
+    ngModelCtrl.$options = {
+      debounce : 500
+    };
+    ngModelCtrl.$options.updateOnDefault = true;
+
+    // Create an empty results array that can be populated with results
+    $scope.results = [];
+
+    var results = angular.element('<div class="search-places-results" ng-show="ngModel"><ul>' +
+      '    <li ng-repeat="result in results" ng-class="{active: isActive($index) }" ng-mouseenter="selectActive($index)" ng-click="selectMatch($index)">' +
+      '    <div><span>{{ result.name }}</span>&nbsp;&nbsp;' +
+      '         <span class="result-vicinity">{{ result.vicinity }}</span>' +
+      '    </div></li>' +
+      '</ul><img class="poweredby-logo" src="//maps.gstatic.com/mapfiles/' +
+      'api-3/images/powered-by-google-on-white2.png"></div>');
+    var $results = $compile(results)($scope);
+    $el.after($results);
+
+    var placesDiv = angular.element('<div id="empty" style="display: none;"></div>');
+    $el.after(placesDiv);
+    var places;
+    $timeout(function() {
+      var emptyDiv = document.getElementById('empty');
+      places = new google.maps.places.PlacesService(emptyDiv);
+    });
+
+    // We want to watch the model for changes and query the PlacesService
+    $scope.$watch("ngModel", function(newVal) {
+      // Don't do any searching if there's no search terms
+      if (!newVal) { return; }
+      if (!$scope.city) {
+        $scope.city = SAN_FRANCISCO;
+      }
+      searchPlaces($scope.city, newVal);
+    });
+
+    /**
+     * Perform a Nearby Search for a particular location using the Google Maps
+     * API PlacesService
+     * @param  {LatLngBounds Obj} location    The location you want to search
+     * @param  {String} searchTerms           What you're searching for
+     */
+    function searchPlaces(location, searchTerms) {
+      // Build the request
+      var request = {
+        bounds: location,
+        keyword: searchTerms
+      };
+      places.nearbySearch(request, buildResults);
+    }
+
+    /**
+     * Build the list of results from the PlacesService search
+     * @param  {[type]} results [description]
+     * @param  {[type]} status  [description]
+     * @return {[type]}         [description]
+     */
+    function buildResults(results, status) {
+      // If the search returns results successfully with no errors
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        // Remove results after the specified limit
+        if (resultsLimit < results.length) {
+          results = results.slice(0, resultsLimit);
+        }
+        $scope.results = results;
+      }
+      // If the search doesn't return results
+      else if (status === google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
+        $scope.results = [{name: 'No results. Try different search terms?'}];
+      }
+    }
+  }
+
   return {
+    restrict: 'A',
     require: 'ngModel',
     scope: {
       ngModel: '=',
       city: '=?' // The city is optional and will default to San Francisco
     },
-    link: function($scope, $el, $attrs) {
-      // Create a placeholder for the results
-      var resultsDiv = '<div class="search-places-results"></div>';
-      // Append the results div to the input
-      $el.after(resultsDiv);
-      // Attach the PlacesService to the element we're creating that will hold
-      // the search results
-      var places = new google.maps.places.PlacesService(resultsDiv);
-      // We want to watch the model for changes and query the PlacesService
-      $scope.watch("ngModel", function(newVal) {
-        // Don't do any searching if there's no search terms
-        if (!newVal) { return; }
-        if (!$scope.city) {
-          $scope.city = SAN_FRANCISCO;
-        }
-
-      });
-
-      /**
-       * Perform a Nearby Search for a particular location using the Google Maps
-       * API PlacesService
-       * @param  {LatLngBounds Obj} location    The location you want to search
-       * @param  {String} searchTerms           What you're searching for
-       */
-      function searchPlaces(location, searchTerms) {
-        // Build the request
-        var request = {
-          bounds: location,
-          keyword: searchTerms
-        };
-        places.nearbySearch(request, buildResults);
-      }
-
-      /**
-       * Build the list of results from the PlacesService search
-       * @param  {[type]} results [description]
-       * @param  {[type]} status  [description]
-       * @return {[type]}         [description]
-       */
-      function buildResults(results, status) {
-        // If the search returns results successfully with no errors
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          console.log("success!");
-        }
-        // If the search doesn't return results
-        else if (status === google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
-          console.log("failure :(");
-        }
-      }
-    }
+    link: link
   };
-});
-
+}]);
 
 // var laSW = new google.maps.LatLng(33.700910, -118.622533)
 // var laNE = new google.maps.LatLng(34.340590, -118.140508)
